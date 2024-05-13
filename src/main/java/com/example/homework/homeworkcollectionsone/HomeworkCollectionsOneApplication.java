1d99b47096ba25aced5ab13c46116211d7701886
CRTXScyNet <CRTXScyNet@gmail.com>
2024-05-13 22:23:06 +0500

    Initial commit

---
+++ src/main/java/com/example/homework/homeworkcollectionsone/HomeworkCollectionsOneApplication.java
@@ -0,0 +1,13 @@
+package com.example.homework.homeworkcollectionsone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkCollectionsOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkCollectionsOneApplication.class, args);
+	}
+
+}
